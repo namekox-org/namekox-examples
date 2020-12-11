be79429ca6ace9b309952f137b4e02cfd178dfdd
李满满 <limanman@corp.netease.com>
2020-12-11 23:30:12 +0800

    :underage:

--- apscheduler/gateway/project/schema/request.py
+++ apscheduler/gateway/project/schema/request.py
@@ -10,4 +10,4 @@ class RequestCreateSchema(Schema):
     path = fields.String(required=True)
     args = fields.String(missing='[]')
     data = fields.String(missing='{}')
-    time = fields.Integer(attribute='__timeout__', missing=None, allow_none=True)
+    time = fields.Float(load_from='__timeout__', missing=None, allow_none=True)
